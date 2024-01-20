856287f62af18570d7f5367d558cbc171f68a0e4
samjwu <sjwu@ualberta.ca>
2024-01-20 15:02:03 -0700

    Generate heat map

--- js/scripts.js
+++ js/scripts.js
@@ -100,5 +100,115 @@ document.addEventListener('DOMContentLoaded', function () {
     request.send();
     request.onload = function () {
         let json = JSON.parse(request.responseText);
+        generateHeatMap(json.baseTemperature, json.monthlyVariance);
+    }
+
+    function generateHeatMap(baseTemperature, data) {
+        data.forEach((d) => {
+            d["year"] = parseTimeYear(d["year"]);
+            d["month"] = parseTimeMonth(d["month"]);
+        });
+
+        let maxScale = d3.max(data, d => d["year"]);
+        let minScale = d3.min(data, d => d["year"]);
+
+        let maxYear = maxScale.getFullYear();
+        let minYear = minScale.getFullYear();
+
+        xScale.domain([minScale, maxScale]);
+
+        yScale.domain(months);
+
+        var xAxis = d3
+            .axisBottom(xScale)
+            .tickFormat(d3.timeFormat("%Y"))
+            .ticks(d3.timeYear.every(10))
+            .tickSizeOuter(0);
+
+        var minX = xScale.domain()[0];
+        var maxX = xScale.domain()[1];
+
+        var tickValues = d3.timeYear.range(minX, maxX, 10);
+
+        tickValues.push(parseTimeYear(1753));
+
+        xAxis.tickValues(tickValues);
+
+        var yAxis = d3
+            .axisLeft(yScale)
+            .tickSizeOuter(0);
+
+        svgContents
+            .append("g")
+            .attr("id", "x-axis")
+            .attr("transform", `translate(0, ${SVG_HEIGHT})`)
+            .call(xAxis);
+
+        svgContents
+            .append("g")
+            .attr("id", "y-axis")
+            .call(yAxis);
+
+        var tooltip = container
+            .append("div")
+            .attr("id", "tooltip");
+
+        svgContents
+            .selectAll("rect")
+            .data(data)
+            .enter()
+            .append("rect")
+            .attr("class", "cell")
+            .attr("data-month", (d) => d["month"].getMonth())
+            .attr("data-year", (d) => d["year"].getFullYear())
+            .attr("data-temp", (d) => d["variance"] + baseTemperature)
+            .on("mouseenter", (event, d) => {
+                tooltip
+                    .attr("data-year", d["year"].getFullYear())
+                    .style("opacity", 1)
+                    .style("left", `${event.layerX}px`)
+                    .style("top", `${event.layerY}px`)
+                    .text(() => {
+                        let year = d["year"].getFullYear();
+                        let month = formatTimeMonth(d["month"]);
+                        let temperature = (d["variance"] + baseTemperature).toFixed(2);
+                        return `Year: ${year}\nMonth: ${month}\nDegrees: ${temperature}`;
+                    });
+            })
+            .on("mouseout", () => {
+                tooltip
+                    .style("opacity", 0);
+            })
+            .attr("x", (d) => xScale(d["year"]))
+            .attr("y", (d) => yScale(formatTimeMonth(d["month"])))
+            .attr("width", SVG_WIDTH / (maxYear - minYear))
+            .attr("height", (SVG_HEIGHT - legendTemperatures.size * 2) / 12)
+            .attr("fill", (d, i) => {
+                let cellTemperature = d.variance + baseTemperature;
+                if (cellTemperature > legendTemperatures.degreesCelsius[0]) {
+                    return legendTemperatures.fillColors[0];
+                }
+                else if (cellTemperature > legendTemperatures.degreesCelsius[1]) {
+                    return legendTemperatures.fillColors[1];
+                }
+                else if (cellTemperature > legendTemperatures.degreesCelsius[2]) {
+                    return legendTemperatures.fillColors[2];
+                }
+                else if (cellTemperature > legendTemperatures.degreesCelsius[3]) {
+                    return legendTemperatures.fillColors[3];
+                }
+                else if (cellTemperature > legendTemperatures.degreesCelsius[4]) {
+                    return legendTemperatures.fillColors[4];
+                }
+                else if (cellTemperature > legendTemperatures.degreesCelsius[5]) {
+                    return legendTemperatures.fillColors[5];
+                }
+                else if (cellTemperature > legendTemperatures.degreesCelsius[6]) {
+                    return legendTemperatures.fillColors[6];
+                }
+                else {
+                    return legendTemperatures.fillColors[7];
+                }
+            })
     }
 });
